c38b21aaa464ba7af4e55047f8af446f3099e907
Arda Oğul Üçpınar <arda@avorna.com>
2018-01-07 01:59:16 +0300

    Still trying

---
+++ Dependency/Dependency.h
@@ -0,0 +1,18 @@
+//
+//  Dependency.h
+//  Dependency
+//
+//  Created by Arda Oğul Üçpınar on 7.01.2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dependency.
+FOUNDATION_EXPORT double DependencyVersionNumber;
+
+//! Project version string for Dependency.
+FOUNDATION_EXPORT const unsigned char DependencyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dependency/PublicHeader.h>
+
+
